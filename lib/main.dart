a7d38b44ac1c358fad6eea1287f5eff8ab5a5efa
Lenovo <sriminu1998@gmail.com>
2023-01-29 21:26:07 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(MaterialApp(
+home: List_with_sepreter(),
+  ));
+}
+
+class List_with_sepreter extends StatelessWidget{
+
+  var name = ['name1', 'name2', 'name4', 'name5','name6','name7','name8','name9'];
+  var phone = [
+    '7896547896',
+    '8965478965',
+    '9987546987',
+    '8845632157',
+    '7896541238',
+    '8795463258',
+    '7459658658','7789652489',
+    '4256978934'
+  ];
+  var images = [
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+  ];
+  var colors = [800,600,500,400,100,200,55,33,400];
+
+  @override
+  Widget build(BuildContext context) {
+ return Scaffold(appBar: AppBar(title: Text('Contacts'),
+
+ ),
+body: ListView.separated(
+
+    itemBuilder: (context,index)
+ {
+
+   return Card(
+     color: Colors.blue[colors[index]],
+     child: ListTile(
+       title: Text(name[index]),
+       subtitle: Text(phone[index]),
+       leading: Image(image: NetworkImage(images[index])),
+
+     ),);
+
+ },
+
+  separatorBuilder: (context, index) {
+  return Divider(thickness: 6, color: Colors.orangeAccent[colors[index]]);
+  },
+ itemCount: name.length),
+
+ );
+  }
+}
